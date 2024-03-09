3da91dfa3eb45848f87a5c57af4a28d9958f97e7
AflahAhamed00 <aflahahamed777@gmail.com>
2024-03-09 09:54:27 +0530

    Added skeleton for the ui to show  before fetching the data

--- src/components/WeatherForcastShimmer.jsx
+++ src/components/WeatherForcastShimmer.jsx
@@ -1,15 +1,26 @@
 import Skeleton from "react-loading-skeleton";
 
 const WeatherForcastShimmer = () => {
+    const arr = [1,2,3,4,5,6,7]
   return (
-   <Skeleton count={8}
-   style={{
-     borderRadius: 10,
-     marginLeft: 10,
-     width: 1500,
-     marginBottom: 10,
-     height:34
-   }}/>
+    arr.map((x,idx)=>(
+        <div key={idx}>
+        <div className="flex justify-between w-12/12 h-10 my-3 ml-4 mr-4 mx-auto rounded-xl shadow-2xl cursor-pointer">
+          <div className="flex items-center">
+          <Skeleton
+            circle
+            style={{ height: 30, width: 30, marginLeft: 10, borderRadius:10 }}
+          />
+          <Skeleton style={{width:100, marginLeft:20, borderRadius:10}}/>
+          </div>
+          <div className="flex items-center mr-4">
+              <Skeleton style={{width:100, borderRadius:10}}/>
+              <Skeleton style={{width:100, borderRadius:10,marginLeft:10}}/>
+          </div>
+        </div>
+      </div>
+    ))
+    
   );
 };
 
--- src/components/WeatherShimmer.jsx
+++ src/components/WeatherShimmer.jsx
@@ -1,18 +1,28 @@
 import Skeleton from "react-loading-skeleton";
 
-
 const WeatherShimmer = () => {
   return (
-    <Skeleton
-      style={{
-        height: 270,
-        width: 300,
-        marginLeft: 600,
-        marginBottom: 20,
-        marginTop: 20,
-        borderRadius: 10,
-      }}
-    />
+    <div className="h-[270px] w-[300px] my-5 mb-14 mx-auto shadow-xl rounded-lg">
+      <div className="flex justify-between items-center p-2">
+        <div className="m-2 p-2 flex justify-between">
+          <div className="m-2 p-2">
+            <Skeleton
+              style={{ marginTop: 30, padding: 4, width: 120, height: 15 }}
+            />
+            <Skeleton style={{ width: 70, height: 10 }} />
+          </div>
+          <Skeleton style={{ width: 100, height: 100, borderRadius: 10 }} />
+        </div>
+      </div>
+      <div className="flex justify-between mx-6">
+        <div className="">
+          <Skeleton  style={{ width: 90, height: 90, borderRadius:10 }} />
+        </div>
+        <div className="ml-16">
+          <Skeleton count={4} style={{ height: 12, width: 100 }} />
+        </div>
+      </div>
+    </div>
   );
 };
 
